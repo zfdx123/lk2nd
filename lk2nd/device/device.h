7c5812eadf0d1364bb57dfccebac1fdf894f00be
Nikita Travkin <nikita@trvn.ru>
2023-01-12 20:57:24 +0500

    lk2nd: device: Expose lk2nd_dev

--- lk2nd/device/device.h
+++ lk2nd/device/device.h
@@ -2,31 +2,7 @@
 #ifndef LK2ND_DEVICE_DEVICE_H
 #define LK2ND_DEVICE_DEVICE_H
 
-struct lk2nd_panel {
-	const char *name;
-	const char *old_compatible;
-	const char *compatible;
-	int compatible_size;
-	const char *ts_compatible;
-};
-
-struct lk2nd_device {
-	const char *compatible;
-	const char *model;
-	const char *battery;
-
-	struct lk2nd_panel panel;
-
-#if WITH_LK2ND_DEVICE_2ND
-	const char *cmdline;
-	const char *bootloader;
-	const char *serialno;
-	const char *device;
-	const char *carrier;
-	const char *radio;
-#endif
-};
-extern struct lk2nd_device lk2nd_dev;
+#include <lk2nd/device.h>
 
 #if WITH_LK2ND_DEVICE_2ND
 const void *lk2nd_device2nd_init(void);
--- lk2nd/include/lk2nd/device.h
+++ lk2nd/include/lk2nd/device.h
@@ -4,6 +4,33 @@
 
 #include <boot.h>
 
+struct lk2nd_panel {
+	const char *name;
+	const char *old_compatible;
+	const char *compatible;
+	int compatible_size;
+	const char *ts_compatible;
+};
+
+struct lk2nd_device {
+	const char *compatible;
+	const char *model;
+	const char *battery;
+
+	struct lk2nd_panel panel;
+
+#if WITH_LK2ND_DEVICE_2ND
+	const char *cmdline;
+	const char *bootloader;
+	const char *serialno;
+	const char *device;
+	const char *carrier;
+	const char *radio;
+#endif
+};
+extern struct lk2nd_device lk2nd_dev;
+
+
 unsigned char *lk2nd_device_update_cmdline(const char *cmdline, enum boot_type boot_type);
 
 bool lk2nd_device2nd_have_atags(void) __PURE;
