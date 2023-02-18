4f61809e2e1f920b4158b177005f6b50826333ea
Nikita Travkin <nikita@trvn.ru>
2023-02-18 16:04:21 +0500

    lk2nd: util: Add version tag generation
    Signed-off-by: Nikita Travkin <nikita@trvn.ru>

--- lk2nd/util/rules.mk
+++ lk2nd/util/rules.mk
@@ -5,3 +5,22 @@ OBJS += \
 	$(LOCAL_DIR)/cmdline.o \
 	$(LOCAL_DIR)/lkfdt.o \
 	$(LOCAL_DIR)/mmu.o \
+
+# Generate the version tag from VCS
+
+LK2ND_VERSION := $(shell $(LK_TOP_DIR)/lk2nd/scripts/do-release.sh -d)
+VERSION_FILE := $(BUILDDIR)/$(LOCAL_DIR)/version.c
+
+.PHONY: $(VERSION_FILE)
+$(VERSION_FILE):
+	@$(MKDIR)
+	@echo generating $@ for lk2nd $(LK2ND_VERSION)
+	@echo "const char* LK2ND_VERSION = \"$(LK2ND_VERSION)\";" > $@
+
+$(BUILDDIR)/$(LOCAL_DIR)/version.o: $(VERSION_FILE)
+	@$(MKDIR)
+	@echo compiling $<
+	$(NOECHO)$(CC) $(CFLAGS) $(THUMBCFLAGS) --std=c99 $(INCLUDES) -c $< -MD -MT $@ -MF $(@:%o=%d) -o $@
+
+OBJS += \
+	$(LOCAL_DIR)/version.o \
